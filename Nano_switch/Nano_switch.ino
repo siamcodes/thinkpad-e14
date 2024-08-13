22085d61ac7e04d2581094ecb77277e2a21974d4
siamcodes <siamcodes@gmail.com>
2024-08-13 21:50:34 +0700

    update

--- Nano_switch/Nano_switch.ino
+++ Nano_switch/Nano_switch.ino
@@ -6,17 +6,19 @@ void setup() {
   Serial.begin(9600);
   pinMode(swA, INPUT);
   pinMode(led, OUTPUT);
+  tone(led, 3000,100);
 }
 
 void loop() {
-  swAstate = digitalRead(swA);
-  if (swAstate == LOW) {
-    Serial.println("swA press");
-    digitalWrite(led, HIGH);
-  } else {
-    Serial.println("swA not press!!");
-    digitalWrite(led, LOW);
-  }
+  
+  // swAstate = digitalRead(swA);
+  // if (swAstate == LOW) {
+  //   Serial.println("swA press");
+  //   digitalWrite(led, HIGH);
+  // } else {
+  //   Serial.println("swA not press!!");
+  //   digitalWrite(led, LOW);
+  // }
 
-  delay(200);
+  // delay(200);
 }
